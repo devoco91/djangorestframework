e9dd793ff196080a720728e0ea2af0e22163d34b
David <abc@gmail.com>
2022-08-25 18:04:57 -1200

    localhost commit

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i2nv#7r&8=u81^#fp-uuhm%fx48iss3#@56v%)r=-st8$x1uqe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'weekdayapi.herokuapp.com']
 
 
 # Application definition
