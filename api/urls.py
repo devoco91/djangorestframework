73078f1c15967aa80a8eed9a24fe473419b57308
David <ashuaib1991@gmail.com>
2025-04-15 12:46:27 +0100

    Altered

--- api/urls.py
+++ api/urls.py
@@ -42,6 +42,7 @@ schema_view = get_schema_view(
    permission_classes=[permissions.AllowAny],
 )
 urlpatterns = [
+    path('', root_view),  # ✅ Root GET `/` now handled
     path('admin/', admin.site.urls),
     path('api/', include('registration.urls')),
     path('api/', include('contact.urls')),
@@ -51,7 +52,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('', root_view),  # ✅ Root GET `/` now handled
+   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
